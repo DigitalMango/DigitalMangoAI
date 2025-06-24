1b884e2208b9c8f7436ea6c2cb5ded7b8b4a0a8d
javiportal <javierandreschavez2003@gmail.com>
2025-06-24 16:10:42 -0600

    Fix GitHub Actions workflow and package.json scripts

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default {
   basePath: isProd ? '/DigitalMangoAI' : '',
-  assetPrefix: isProd ? '/DigitalMangoAI/' : '',
+  assetPrefix: isProd ? '/DigitalMangoAI' : '',
   output: 'export', // for static export
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
