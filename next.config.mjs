a20b43f775bb30d63e167717c30e9e1b9ee5bae0
javiportal <javierandreschavez2003@gmail.com>
2025-06-24 17:09:56 -0600

    fix: elimina output export para compatibilidad con Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 export default {
-  basePath: isProd ? '/DigitalMangoAI' : '',
-  assetPrefix: isProd ? '/DigitalMangoAI' : '',
-  output: 'export', // for static export
   trailingSlash: true,
   images: {
     unoptimized: true
